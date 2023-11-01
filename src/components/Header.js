8d7f8f1afc54bbffd1891153d2f5f0cf3d17b02c
Manohar Kumar <manohar.csengineer@gmail.com>
2023-11-01 23:58:09 +0530

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+const Header = () => {
+  return (
+    <div className="container">
+    <div className="header">
+   <div> <h1>Record Keeping Website</h1></div>
+    <div><h2>By Manohar Kumar</h2></div>
+    </div>
+    </div>
+  )
+}
+
+export default Header
